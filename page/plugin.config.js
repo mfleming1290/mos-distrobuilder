fbd7f5bb6c29c6769de67eada3155d68ed84b3b0
Matt Fleming <matthewjamesfleming@gmail.com>
2026-09-18 20:39:05 -0700

    fix distrobuilder directory definitions

--- page/plugin.config.js
+++ page/plugin.config.js
@@ -2,9 +2,10 @@ export default {
   name: 'distrobuilder',
   displayName: 'Distrobuilder',
   description: 'Build custom LXC images directly on MOS using distrobuilder.',
-  version: '0.1.2',
+  version: '0.1.3',
   icon: '',
   author: 'mfleming1290',
   homepage: '',
   widget: false,
 };
+
